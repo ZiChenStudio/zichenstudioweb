272703757b6f08bb5e66ba14a714c23729bb78f8
ZiChenStudio <zichenstudio@outlook.com>
2022-12-30 16:42:59 +0800

    Update 404.html

--- js/run.js
+++ js/run.js
@@ -1,10 +1,9 @@
 $(function () {
     if ($('#goto').text() != '1IxOnYB6ND3fieK8_I458wBMlMLsiyvKDUKxuZIa0eH97jY5gL-DJxi1ECXqucoy8IlGwB2hrlhj1xP-gfypQhAtDjlKcna2UoEcGv9UtSIUDDnbd2HHUu18wyYQbcRXsdXpIGl6M6Gwop_-DSAUesKdyx5yK8untBi9gI_3wzyYgX1o470Por2gGIhE9LKb7CNEdk0a9iZM8lbeK73KDEA1mSlxptRhH84ICrai70tD_gefyMuFLjOlFhmz4UE0') {
-        $('#goto').remove();
         return;
     }
     else {
-        $('#goto').remove();
+        alert('PASS')
     }
     function html() {
         $("#ul").hide();
