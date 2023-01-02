eb12f415ab30de84924e18b7da92e24ce7c47213
ZiChenStudio <zichenstudio@outlook.com>
2023-01-02 12:47:47 +0800

    Update html

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+var nav = "<ul>\
+<li><span><a href=\"/index.html\">首页</a></span></li>\
+<li><span><a href=\"/blog/index.html\">Blog</a></span></li>\
+<li><span><a href=\"/auot/index.html\">公告</a></span></li>\
+<li><span><a href=\"/html/software.html\">软件</a></span></li>\
+<li><span><a href=\"/html/project.html\">项目</a></span></li>\
+<li><span><a href=\"/html/contact.html\">联系我们</a></span></li>\
+<li><span><a href=\"/html/legal.html\">法律声明</a></span></li>\
+</ul>"
+$('.nav').append(nav);
